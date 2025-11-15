a25db0f59ffead8799d3871b9270e388d4732763
Ilwyd <wgray5093@gmail.com>
2025-11-15 19:34:04 -0400

    Add space to log string

--- plugin/helpers.lua
+++ plugin/helpers.lua
@@ -10,7 +10,7 @@ end
 local function log(msg)
 	local _, _, _, hour, minute, second = bolt.datetime()
 
-	print(string.format("[%02d:%02d:%02d]", tostring(hour), tostring(minute), tostring(second)) .. msg)
+	print(string.format("[%02d:%02d:%02d]", tostring(hour), tostring(minute), tostring(second)) .. " " .. msg)
 end
 
 local function tprint(tbl, indent)
